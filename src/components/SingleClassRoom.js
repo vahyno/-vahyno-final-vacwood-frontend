8fbf700b17e8524e99eef6886506ca82610928b9
vahyno <vahyno@gmail.com>
2018-05-28 12:16:58 -0700

    updated styling added comments to classroom

--- src/components/SingleClassRoom.js
+++ src/components/SingleClassRoom.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ClassRoomsModel from '../models/ClassRoomsModel';
+import '../styles/singleClassroom.css';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -23,20 +24,41 @@ class SingleClassRoom extends Component {
     }
 
     render(){
-        let classroom = this.state.classroom === null ? <h2>Loading...</h2> : this.state.classroom
-        console.log(this.state.classroom)
+        let singleClassroom = this.state.classroom === null ? <h2>Loading...</h2> : this.state.classroom
+        console.log(this.state.classroom);
 
+        let classroomComments = this.state.classroom === null ? null : this.state.classroom.comments
+            .map( comment => {
+                console.log(comment);
+                // let datenumber = parseInt(comment.created_at.replace( /\D+/g, ''));
+                let formatedCreated_at = `${comment.created_at.slice(0,10)} at ${comment.created_at.slice(11,19)}`
+                return (
+                    <div className="comment" key={comment._id}>
+                        <div className="card">
+                            <div className="created_at">{ formatedCreated_at }</div>
+                            <div className="card-body">{ comment.content }
+                                <button  className="commentButton btn-flat btn-small waves-effect waves-light red right">X</button>
+                                <button  className="commentButton btn-flat btn-small waves-effect waves-light green right">reply</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
 
         return (
             <div>
                 <Header/>
-                <h4 className="center-align"> { classroom.title } </h4>
-                <h4 className="center-align">teacher: { classroom.teacher } </h4>
+                <h4 className="center-align"> { singleClassroom.title } </h4>
+                <h4 className="center-align">teacher: { singleClassroom.teacher } </h4>
                 <Link to ={`/classrooms`} className="col s12 m7">
-                <img src={ classroom.image_url } alt={classroom.title} className="hoverable singleClassroomImg"/>
+                <img src={ singleClassroom.image_url } alt={singleClassroom.title} className="hoverable singleClassroomImg"/>
                 </Link>
-                
 
+                <h6>here will be comment form</h6>
+
+                {/* end of comment form */}
+
+                { classroomComments }               
 
                 <Footer/>
             </div>
