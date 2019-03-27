a678b388e30347d3f7f91f7e476bbf191bbe22cb
vahyno <vahyno@gmail.com>
2019-03-26 22:13:37 -0700

    create functional component ClassRoom and refactor ClassRooms

---
+++ src/components/ClassRoom.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../styles/classrooms.css';
+
+const ClassRoom = (props) => {
+    const { title, teacher, image_url, info} = props.classroom
+
+    return (
+        <div className="card">
+            <div className="card-image">
+                <h4 className="center-align">{ title }</h4>
+                <h4 className="center-align">{ teacher }</h4>
+                <img src={ image_url } alt={ image_url }/>
+            </div>
+            <div className="card-content">
+                <p> { info } </p>
+            </div>
+        </div>
+    )
+}
+
+export default ClassRoom;
+
--- src/components/ClassRooms.js
+++ src/components/ClassRooms.js
@@ -5,12 +5,13 @@ import ClassRoomsModel from '../models/ClassRoomsModel';
 import '../styles/classrooms.css';
 import Header from './Header';
 import Footer from './Footer';
+import Classroom from './ClassRoom';
 
 
 class ClassRooms extends Component {
 
     state = {
-        results: null,
+        results : null,
     }
 
     componentDidMount() {
@@ -25,33 +26,45 @@ class ClassRooms extends Component {
 
     render (){
 
-        let results = this.state.results === null ? null : this.state.results
-            .map(classroom => {
-                console.log("1 Classroom: ", classroom);
-                return (
-                    <div className="row allclassrooms card-action hoverable" key={classroom._id}>
-                        <Link to ={`/classrooms/${classroom._id}`} className="col s12 m7">
-                            <div className="card">
-                                <div className="card-image">
-                                    <h4 className="center-align">{ classroom.title }</h4>
-                                    <h4 className="center-align">{ classroom.teacher }</h4>
-                                    <img src={ classroom.image_url } alt={ classroom.image_url }/>
-                                </div>
-                                <div className="card-content">
-                                    <p> { classroom.info } </p>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>        
-                );
-            }); 
+        const { results } = this.state;
+        console.log('results', results)
+        // let results = this.state.results === null ? null : this.state.results
+        //     .map(classroom => {
+        //         console.log("1 Classroom: ", classroom);
+        //         return (
+        //             <div className="row allclassrooms card-action hoverable" key={classroom._id}>
+        //                 <Link to ={`/classrooms/${classroom._id}`} className="col s12 m7">
+        //                     <div className="card">
+        //                         <div className="card-image">
+        //                             <h4 className="center-align">{ classroom.title }</h4>
+        //                             <h4 className="center-align">{ classroom.teacher }</h4>
+        //                             <img src={ classroom.image_url } alt={ classroom.image_url }/>
+        //                         </div>
+        //                         <div className="card-content">
+        //                             <p> { classroom.info } </p>
+        //                         </div>
+        //                     </div>
+        //                 </Link>
+        //             </div>        
+        //         );
+        //     }); 
 
         return (
             <div>
             <Header/>
             <div className="allClassroomsContainer blue accent-1">
                 <div className="row center-cols center-align">
-                 { results } 
+                    { results !== null && (
+                        results.map((classroom) => (
+                            <div 
+                                className="row allclassrooms card-action hoverable"
+                                key={classroom._id}>
+                                <Link to ={`/classrooms/${classroom._id}`} className="col s12 m7">
+                                    <Classroom classroom={classroom} /> 
+                                </Link>    
+                            </div>
+                        )))
+                    } 
                 </div>
             </div>
             <Footer/>
