d3dc5f9fb0d74394329f19e47791730621af621c
vahyno <vahyno@gmail.com>
2019-03-29 23:16:36 -0700

    conver to redux step1 - add comment action, reducer

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,33 +1,54 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 import Comment from './Comment';
 
 class Comments extends Component {
     render(){
-        const {commentsData, deleteComment, responseToComment, handleResponseCommentForm, submitReplyComment} = this.props;
-        if (!commentsData) {
-            return (<div>...loading</div>);
-        }
+        // const {commentsData, deleteComment, responseToComment, handleResponseCommentForm, submitReplyComment} = this.props;
+        // if (!commentsData) {
+        //     return (<div>...loading</div>);
+        // }
 
-        console.log('classRoom data in Comments: ', this.props.commentsData.comments); 
-        const comments = commentsData.comments
-
+        // console.log('classRoom data in Comments: ', this.props.commentsData.comments); 
+        // const comments = commentsData.comments
+        // console.log('COMMENTCOMMENTCOMMENTCOMMENTCOMMENT: ',this.props)
+        const { commentIDs, classroomId } = this.props;
+        // console.log('!!!!!!!GOLD!!!!!!', commentIDs, 'classroomId: ', classroomId);
         return (
             <div>
-                {comments.map(comment => (
+                {/* {JSON.stringify(commentIDs)} */}
+                {/* {comments.map(id => (
                     <Comment 
                         classroom={commentsData}
-                        key={comment._id}
+                        key={id}
                         comment={comment}
                         deleteComment={deleteComment}
                         responseToComment={responseToComment}
                         handleResponseCommentForm={handleResponseCommentForm}
                         submitReplyComment={submitReplyComment}
                         />
+                ))} */}
+                {commentIDs.map(id => (
+                    <Comment
+                        key={ id }
+                        commentId={ id }
+                        classroomId={ classroomId } 
+                    />
                 ))}
             </div>
         )        
     }
 }
 
-export default Comments;
+function mapStateToProps({ classrooms }, props) {
+    const { classroomId} = props;
+    return {
+        classroomId,
+        commentIDs: !classrooms[classroomId]
+            ? []
+            : classrooms[classroomId].comments.map(comment => comment._id),
+    }
+}
+
+export default connect(mapStateToProps)(Comments);
--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,31 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import ClassRoomsModel from '../models/ClassRoomsModel';
+import { handleCreateComment } from '../actions/classroom';
 
-const MessageForm = (props) => {
-    const {onSubmit, handleCommentForm, newComment} = props;
-    return (
-        <div className="row comment_form">
-            <form className="col s12" onSubmit={onSubmit}>
-                <div className="row">
-                    <div className="input-field col s6">
-                        <textarea cols="40" rows="10" onInput={handleCommentForm}
-                        value={newComment}
-                        placeholder="Write your message!"
-                        id="comment"
-                        type="text"
-                        className="validate" required/>
+
+
+class MessageForm extends Component {
+    state = {
+        commentText: '',
+    }
+
+    handleCommentForm = (event) => {
+        let newComment = event.target.value;
+        this.setState({
+            commentText: newComment
+        })
+        // console.log('handleCommentForm => newComment', newComment);
+    }
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        const { classroomId, dispatch } = this.props;
+        const { commentText } = this.state;
+
+        dispatch(handleCreateComment(classroomId, commentText));
+        this.setState(()=> ({
+            commentText: ''
+        }));
+
+        // ClassRoomsModel.newComment(classroomId, commentText)
+        // .then(newComment => {
+        //     this.setState({
+        //         classroom: {
+        //             ...this.state.classroom,
+        //             comments: this.state.classroom.comments.concat(newComment.data),
+        //         },
+        //         newComment: '',
+        //     });
+        // });
+        // console.log(this.state)
+    }
+
+    render() {
+        const { commentText } = this.state;
+        return (
+            <div className="row comment_form">
+                <form className="col s12" onSubmit={this.onFormSubmit}>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <textarea cols="40" rows="10" onChange={this.handleCommentForm}
+                            value={commentText}
+                            placeholder="Write your message!"
+                            id="comment"
+                            type="text"
+                            className="validate" required/>
+                        </div>
                     </div>
-                </div>
-                <button 
-                    className="commentNewButton btn-flat btn-small waves-effect waves-light blue accent-1 right" 
-                    type="submit" 
-                    name="action">
-                        New Message
-                </button>
-            </form>
-            {/* { classroomCommentReplies } */}
-        </div>
-    )
+                    <button 
+                        className="commentNewButton btn-flat btn-small waves-effect waves-light blue accent-1 right" 
+                        type="submit" 
+                        name="action">
+                            New Message
+                    </button>
+                </form>
+                {/* { classroomCommentReplies } */}
+            </div>
+        )
+    }
 }
 
-export default MessageForm;
+function mapStateToProps (state,{ classroomId }) {
+    return {
+        classroomId,
+    }
+}
+
+export default connect(mapStateToProps)(MessageForm);
 
